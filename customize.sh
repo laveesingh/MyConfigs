9a240e0b6eea014c4b3804e44818db8e61dcf1fa
laveesingh <laveechaudharytheone@gmail.com>
2017-10-31 12:02:17 +0530

    add: started fresh customize.sh added a few commands

--- customize.sh
+++ customize.sh
@@ -1,3 +1,41 @@
+# Install vim & tmux
+sudo apt-get install vim -y
+sudo apt-get install tmux -y
+
+# putting in configurations
+if [ ! -d "~/Documents/MyConfigs" ]; then
+  git clone https://github.com/laveesingh/MyConfigs.git ~/Documents/MyConfigs
+fi
+ln -s ~/Documents/MyConfigs/.vimrc ~
+ln -s ~/Documents/MyConfigs/.tmux.conf ~
+ln -s ~/Documents/MyConfigs/.bashrc ~
+ln -s ~/Documents/MyConfigs/.bash_aliases ~
+
+# Install pathogen vim package manager
+echo "installing pathogen..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+
+# Install sublime colorschmes for vim
+echo "installing molokai colorscheme for vim"
+cd ~/.vim
+git clone https://github.com/tomasr/molokai.git
+git clone https://github.com/sickill/vim-monokai.git
+mkdir -p colors
+mv molokai/colors/molokai.vim colors/
+mv vim-monokai/colors/monokai.vim colors/
+rm -rf molokai/
+rm -rf vim-monokai/
+
+
+# Install vim plugins
+cd ~/.vim/bundle && git clone https://github.com/scrooloose/nerdcommenter.git
+
+
+
+
+
 
 # Install vim plugins
 cd ~/.vim/bundle/ && git clone https://github.com/mxw/vim-jsx.git
@@ -15,3 +53,4 @@ sudo apt-get install mono-xbuild
 
 # vim-syntastic extensions
 sudo npm install -g eslint babel-eslint eslint-plugin-react syntastic-react jshint react-tools
+
