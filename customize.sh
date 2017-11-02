71ba88a584d5dd89a951ac470df478c7e701ea48
laveesingh <laveechaudharytheone@gmail.com>
2017-11-03 01:14:51 +0530

    mod: modify customize file to make more favourable changes

--- customize.sh
+++ customize.sh
@@ -12,13 +12,13 @@ ln -s ~/Documents/MyConfigs/.bashrc ~
 ln -s ~/Documents/MyConfigs/.bash_aliases ~
 
 # Install pathogen vim package manager
-echo "installing pathogen..."
+#echo "installing pathogen..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 
 # Install sublime colorschmes for vim
-echo "installing molokai colorscheme for vim"
+#echo "installing molokai colorscheme for vim"
 cd ~/.vim
 git clone https://github.com/tomasr/molokai.git
 git clone https://github.com/sickill/vim-monokai.git
@@ -27,13 +27,19 @@ mv molokai/colors/molokai.vim colors/
 mv vim-monokai/colors/monokai.vim colors/
 rm -rf molokai/
 rm -rf vim-monokai/
+cd  # return back to home directory
 
 
 # Install vim plugins
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 git clone https://github.com/Raimondi/delimitMate.git ~/.vim/bundle/delimitMate
 git clone https://github.com/terryma/vim-multiple-cursors.git  ~/.vim/bundle/vim-multiple-cursors
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
 git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
+git clone https://github.com/SirVer/ultisnips ~/.vim/bundle/ultisnips
 
+# install YouCompleteMe for vim
+cd ~/.vim/bundle && git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.py
